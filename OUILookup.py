511515299ba81828de2c0e48b0ba185116cb4ccb
JoaquinAmigo <joaquin.amigo@alumnos.uv.cl>
2023-10-24 01:00:39 +0000

    edicion de codigo

--- OUILookup.py
+++ OUILookup.py
@@ -1,23 +1,33 @@
+#!/usr/bin/env python3
+
 import subprocess
 import getopt
 import sys
-import ipaddress #Con esto podremos verificar si la ip pertenece a la misma red o no
 
 #Archivo para la base de datos de manofacturadores.
-DATA_FILE = "manuf.txt"
+DATA_FILE = "/home/joak/Tarea02-Amigo-Torres-Joaquin/manuf"
+NETWORK = "192.168.1."
 
 # Función para obtener los datos de fabricación de una tarjeta de red por IP
 def obtener_datos_por_ip(ip):
-	try:
-		mac_info = subprocess.check_output(["arp", "-n", ip]).decode("utf-8")
-		lineas = mac_info.split("\n")
-		if len(lineas) > 1:
-			mac = lineas[1].split()[2]
-			obtencion_datos_mac(mac)
-		else:
-			print(f"No se encontro dirección MAC la IP {ip}")
-	except Exception as e:
-		print(f"Error al obtener la direccion MAC: {e}")
+	if ip.startswith(NETWORK):
+		try:
+			mac_info = subprocess.check_output(["arp", "-n", ip]).decode("utf-8")
+			lines = mac_info.split("\n")
+			if len(lines) > 1:
+				mac_line=lines[1]
+				if len(mac_line.split()) > 2:
+					mac = mac_line.split()[2]
+					obtencer_datos_por_mac(mac)
+				else:
+					print(f"No se encontro dirección MAC la IP {ip}")
+			else:
+				print(f"No se encontro dirección MAC la IP {ip}")
+		except Exception as e:
+			print(f"Error al obtener la direccion MAC: {e}")
+	else:
+		print(f"Error: IP está fuera de la red del host")
+
     # Implementa la lógica para obtener los datos por IP aquí
    # print("Aqui su codigo para obtener los datos por ip")
    # pass
@@ -26,9 +36,9 @@ def obtener_datos_por_ip(ip):
 def obtener_datos_por_mac(mac):
 	try:
 		with open(DATA_FILE, "r") as file:
-			lineas = file.readlines()
-			for linea in lineas:
-				if linea.startswith(mac):
+			lines = file.readlines()
+			for line in lines:
+				if line.startswith(mac):
 					manofacturadores = linea.split("\t")[1]
 					print(f"MAC Address : {mac}")
 					print(f"Fabricante : {manofacturadores}")
@@ -64,7 +74,7 @@ def main(argv):
 	try:
 		opts, args = getopt.getopt(argv, "i:m:a", ["ip=", "mac=", "arp"])
 
-	except getopt.GeetoptError
+	except getopt.GetoptError:
 		print("Uso: python OUILookup.py --ip <IP> | --mac <MAC> | --arp")
 		sys.exit(2)
 
@@ -76,16 +86,13 @@ def main(argv):
 		elif opt in ("-a", "--arp"):
 			mostrar_arp = True
 	if ip:
-		if ipaddress.IPv4Address(ip) in ipaddress.IPv4Network("192.168.1.30/24"):
-			obtener_datos_por_ip(ip)
-		else:
-			print("Error: La IP esta fuera de la red del host")
+		obtener_datos_por_ip(ip)
 	elif mac:
 		obtener_datos_por_mac(mac)
 	elif mostrar_arp:
 		obtener_tabla_arp()
 	else:
-		print("Debe proporcionar una opción válida (-i, -m, -a).")
+		print("Debe proporcionar una opción válida (--ip, --mac, --arp).")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
